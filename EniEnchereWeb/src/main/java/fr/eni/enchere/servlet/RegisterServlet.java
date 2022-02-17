0e325b1734915a040945f461983af4f723fd3d36
val-denale <99645437+val-denale@users.noreply.github.com>
2022-02-17 17:01:22 +0100

    deleted import

--- EniEnchereWeb/src/main/java/fr/eni/enchere/servlet/RegisterServlet.java
+++ EniEnchereWeb/src/main/java/fr/eni/enchere/servlet/RegisterServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import fr.eni.enchere.bll.UtilisateurManager;
 
-import fr.eni.enchere.bll.UtilisateurManager;
 import fr.eni.enchere.bo.Utilisateur;
 
 
