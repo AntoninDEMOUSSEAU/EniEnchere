feeeb135e5dbc7dcc3a5520a1a9e66f362132f57
AxelitoC <castagnieaxel@gmail.com>
2022-02-17 16:58:18 +0100

    Dark sasuke 2.0

--- EniEnchereWeb/src/main/java/fr/eni/enchere/servlet/RegisterServlet.java
+++ EniEnchereWeb/src/main/java/fr/eni/enchere/servlet/RegisterServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import fr.eni.enchere.bll.UtilisateurManager;
 
-import fr.eni.echere.bll.UtilisateurManager;
+import fr.eni.enchere.bll.UtilisateurManager;
 import fr.eni.enchere.bo.Utilisateur;
 
 
